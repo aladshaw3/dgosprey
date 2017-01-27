d09edaa1e3f8fe49ccd049ee1fc2fd98a148bb19
aladshaw3 <aladshaw3@gatech.edu>
2017-01-27 14:59:37 -0500

    Updated moose and ran tests.

--- include/base/DgospreyRevision.h
+++ include/base/DgospreyRevision.h
@@ -3,6 +3,6 @@
 #ifndef DGOSPREY_REVISION_H
 #define DGOSPREY_REVISION_H
 
-#define DGOSPREY_REVISION "git commit a1a9018 on 2017-01-17"
+#define DGOSPREY_REVISION "git commit 0ffeec8 on 2017-01-24"
 
 #endif // DGOSPREY_REVISION_H
