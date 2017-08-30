68606a0ca9a5c36ec7615e2835e3ad80c35a1edf
Alex Wiechert <awiechert3@gmail.edu>
2017-08-30 14:40:17 -0400

    Two Block Update

--- include/base/DgospreyRevision.h
+++ include/base/DgospreyRevision.h
@@ -3,11 +3,7 @@
 #ifndef DGOSPREY_REVISION_H
 #define DGOSPREY_REVISION_H
 
-<<<<<<< HEAD
-#define DGOSPREY_REVISION "git commit be39ba4 on 2017-07-12"
-#define DGOSPREY_VERSION "be39ba4"
-=======
-#define DGOSPREY_REVISION "git commit 8499a23 on 2017-08-25"
->>>>>>> 0d37e4dcbdc095294dcaa31b776789ce1f145b8d
+#define DGOSPREY_REVISION "git commit cd5ec83 on 2017-08-28"
+#define DGOSPREY_VERSION "cd5ec83"
 
 #endif // DGOSPREY_REVISION_H
