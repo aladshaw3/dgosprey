f95ec76a4afa9afd051da87c8b4f27aab9df6e1a
aladshaw3 <aladshaw3@gatech.edu>
2017-11-17 16:07:43 -0500

    Fixed tests for new dispersion estimate.

--- include/base/DgospreyRevision.h
+++ include/base/DgospreyRevision.h
@@ -3,6 +3,6 @@
 #ifndef DGOSPREY_REVISION_H
 #define DGOSPREY_REVISION_H
 
-#define DGOSPREY_REVISION "git commit 0a31f82 on 2017-11-16"
+#define DGOSPREY_REVISION "git commit 7f2053a on 2017-11-17"
 
 #endif // DGOSPREY_REVISION_H
